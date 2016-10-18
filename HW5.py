8c9c588ad87f0d7b2205b192cea06d84f92341c2
Dan Griffin <unknown7979@gmail.com>
2016-10-18 12:52:05 -0500

    Initial commit.

---
+++ HW5.py
@@ -0,0 +1,38 @@
+import numpy as np
+import numpy.linalg as linalg
+
+
+
+
+
+
+def part2():
+	pass
+
+
+def part3():
+	pass
+
+
+def part4():
+	pass
+
+
+
+
+
+
+
+
+
+def main():
+	part2()
+	part3()
+	part4()
+
+
+
+
+if __name__ == '__main__':
+	main()
+
